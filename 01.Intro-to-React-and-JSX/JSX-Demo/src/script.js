d18db591b068852433cd48c17aa5dbae77ec4d32
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-10-24 09:33:39 +0300

    JSX demo

---
+++ 01.Intro-to-React-and-JSX/JSX-Demo/src/script.js
@@ -0,0 +1,26 @@
+import '../node_modules/react/umd/react.production.min.js';
+import '../node_modules/react-dom/umd/react-dom.production.min.js';
+
+const roomDomElement = document.getElementById('root');
+
+const root = ReactDOM.createRoot(roomDomElement);
+
+const reactHeading = React.createElement('h1', {}, 'Hello from JSX!');
+const reactSecondHeading = React.createElement('h2', {}, 'The best syntax ever!');
+const header = React.createElement(
+    'header', 
+    { className: 'site-header'},
+     reactHeading, 
+    reactSecondHeading
+);
+
+const headerJSX = (
+    <header className='site-header'>
+        <h1>Hello from JSX!</h1>
+        <h2>The best syntax ever!</h2>
+
+        <p>something else here</p>
+    </header>
+);
+
+root.render(headerJSX);
