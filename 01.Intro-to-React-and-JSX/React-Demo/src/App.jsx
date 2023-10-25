7330528e359487402168ba18b4b2a14967f01ebe
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-10-26 00:06:22 +0300

    React app demo started

---
+++ 01.Intro-to-React-and-JSX/React-Demo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1> My First React Application</h1>
+      </div>
+
+      <h1>Vite + React = best combo</h1>
+    </div>
+  )
+}
+
+export default App
