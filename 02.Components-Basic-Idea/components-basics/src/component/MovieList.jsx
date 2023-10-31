885772190e7170b2aa155529834ef598b7fca1c5
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-11-01 00:05:42 +0200

    switching from laptop to desktop

---
+++ 02.Components-Basic-Idea/components-basics/src/component/MovieList.jsx
@@ -0,0 +1,13 @@
+export default function MovieList(props){
+    return (
+        <div className="movie-list">
+            <h2>Movie List</h2>
+
+            <ul>
+                <li>First movie</li>
+                <li>Second movie</li>
+                <li>Third movie</li>
+            </ul>
+        </div>
+    )
+}
