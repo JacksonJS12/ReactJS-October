11c396b42039ecbf4089b5cf109f0f23575c1e5a
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-11-12 21:59:11 +0200

    done

---
+++ 02.Components-Basic-Idea/components-basics/src/component/Timer.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function Timer(props){
+    const [time, setTime] = useState(props.startTime);
+
+    //let time = 0;
+    console.log(`Current time is = ${time}`)
+
+    setTimeout(() => {
+        setTime(time + 1);
+        console.log(`Current time is = ${time}`)
+    }, 1000);
+    return(
+        <div>
+            <h3>Timer</h3>
+
+            <p>{time}</p>
+        </div>
+    )
+}
