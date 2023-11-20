d41e7655d9d70793cd7216ff7a3cc495148242e1
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-11-20 23:31:16 +0200

    commit.

---
+++ 04.Workshop-Components/client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        This site is designed to be used htmlFor training purposes at SoftUni.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
